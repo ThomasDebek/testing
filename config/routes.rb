1c3b06836088c128b90b837dde259f8d8706d9bc
ThomasDebek <tom.debek@gmail.com>
2014-07-09 20:10:59 +0200

    zabawa

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  
+  root 'static_pages#home'
 
+  
+
+  # get 'static_pages/home'
   get 'static_pages/help'
-
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
